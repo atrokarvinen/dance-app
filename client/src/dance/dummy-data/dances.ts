33be95d41ce3c8c6c0bb0b52ea153e524a6a1de2
Atro Karvinen <atrok@hotmail.com>
2024-07-16 10:43:13 +0300

    removed dummy data

--- client/src/dance/dummy-data/dances.ts
+++
@@ -1,7 +0,0 @@
-import { Dance } from "../dance";
-import { tangoBasicStep, waltzBasicStep } from "./dance-patterns";
-
-export const dummyDances: Dance[] = [
-  { danceId: 1, name: "Waltz", dancePatterns: [waltzBasicStep] },
-  { danceId: 2, name: "Tango", dancePatterns: [tangoBasicStep] },
-];
