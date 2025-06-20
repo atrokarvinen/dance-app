803365201b15f5ecda4835d71dd361018cd234fe
Atro Karvinen <atrok@hotmail.com>
2025-06-20 10:13:51 +0300

    removed analyzer

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { analyzer } from "vite-bundle-analyzer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), analyzer()],
+  plugins: [react()],
 });
